20e2a6b8a0959d34b268c085cbfa494082027a18
BiroNora <52859737+BiroNora@users.noreply.github.com>
2023-08-10 20:01:54 +0200

    wip

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import '@picocss/pico'
+  </script>
+
+<nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/register/school">School</a>
+      </li>
+    </ul>
+  </nav>
+
+<slot />
