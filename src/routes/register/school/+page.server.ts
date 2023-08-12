dc2fc4b5ac6bf1d289cf04c6a539c23f99d5d734
BiroNora <52859737+BiroNora@users.noreply.github.com>
2023-08-12 14:49:44 +0200

    School register

---
+++ src/routes/register/school/+page.server.ts
@@ -0,0 +1,143 @@
+import { fail, redirect} from '@sveltejs/kit'
+import type { Action, Actions, PageServerLoad } from './$types'
+import { db } from '$lib/database'
+
+
+export const load: PageServerLoad = async () => {
+  const country = await db.country.findMany({
+    orderBy: { name: 'asc' },
+  })
+  const regio = await db.region.findMany({
+    orderBy: { name: 'asc' },
+  })
+  const county = await db.county.findMany({
+    orderBy: { name: 'asc' },
+  })
+  const city = await db.city.findMany({
+    orderBy: { name: 'asc' },
+  })
+  return {country, regio, county, city}
+}
+
+const school: Action = async ({ request }) => {
+  const data = await request.formData()
+  const country_id = Number(String(data.get('countr')))
+  const region_id = Number(String(data.get('region')))
+  const county_id = Number(String(data.get('county')))
+  const city_id = Number(String(data.get('city')))
+  const om_id = String(data.get('om')) || null
+  const name = String(data.get('name'))
+  const zip_code = String(data.get('zip'))
+	const address = String(data.get('address'))
+  const dir_name = String(data.get('dirname'))
+  const dir_phone = String(data.get('dirphone'))
+  const school_email = String(data.get('email'))
+  const website = String(data.get('website')) || null
+  const altisk = Boolean(data.get('iskA'))
+  const gimn = Boolean(data.get('iskB'))
+  const szakgimn = Boolean(data.get('iskC'))
+  const szakkoz = Boolean(data.get('iskD'))
+  const szakisk = Boolean(data.get('iskE'))
+  const techn = Boolean(data.get('iskF'))
+  const szakkepz = Boolean(data.get('iskG'))
+  const almuv = Boolean(data.get('iskH'))
+  const muvokt = Boolean(data.get('iskI'))
+  const keszseg = Boolean(data.get('iskJ'))
+  const fejl = Boolean(data.get('iskK'))
+  const kieg = Boolean(data.get('iskL'))
+  const kolleg = Boolean(data.get('iskM'))
+  const hidp = Boolean(data.get('iskN'))
+  const coop = Boolean(data.get('coop'))
+	const note = String(data.get('message'))
+  const contact_id = 'norabora.biro@gmail.com'
+  const school_type = []
+
+  if (altisk) {
+    school_type.push('ÁLTALÁNOS ISKOLA')
+  }
+  if (gimn) {
+    school_type.push('GIMNÁZIUM')
+  }
+  if (szakgimn) {
+    school_type.push('SZAKGIMNÁZIUM')
+  }
+  if (szakkoz) {
+    school_type.push('SZAKKÖZÉPISKOLA')
+  }
+  if (szakisk) {
+    school_type.push('SZAKISKOLA')
+  }
+  if (techn) {
+    school_type.push('TECHNIKUM')
+  }
+  if (szakkepz) {
+    school_type.push('SZAKKÉPZŐ ISKOLA')
+  }
+  if (almuv) {
+    school_type.push('ALAPFOKÚ MŰVÉSZETOKTATÁS')
+  }
+  if (muvokt) {
+    school_type.push('MŰVÉSZETI OKTATÁS')
+  }
+  if (keszseg) {
+    school_type.push('KÉSZSÉGFEJLESZTÉS')
+  }
+  if (fejl) {
+    school_type.push('FEJLESZTŐ NEVELÉS-OKTATÁS')
+  }
+  if (kieg) {
+    school_type.push('KIEGÉSZÍTŐ NEMZETISÉGI NYELVOKTATÁS')
+  }
+  if (kolleg) {
+    school_type.push('KOLLÉGIUM')
+  }
+  if (hidp) {
+    school_type.push('HÍDPROGRAMOK')
+  }
+
+  const regioncountry = await db.region.findUnique({
+    where: { region_id }
+  })
+  if (regioncountry?.country_id != country_id) {
+    return fail(400, {school: true})
+  }
+
+  const countyregion = await db.county.findUnique({
+    where: { county_id }
+  })
+  if (countyregion?.region_id != region_id) {
+    return fail(400, {school: true})
+  }
+
+  const citycounty = await db.city.findUnique({
+    where: { city_id }
+  })
+  if (citycounty?.county_id != county_id) {
+    return fail(400, {school: true})
+  }
+
+  await db.school.create({
+    data: {
+      om_id,
+      name,
+      zip_code,
+      address,
+      contact_id,
+      dir_name,
+      dir_phone,
+      school_email,
+      website,
+      school_type,
+      coop,
+      note,
+      city_id,
+      country_id,
+      county_id,
+      region_id
+    }
+  })
+
+  throw redirect(303, '/login')
+}
+
+export const actions: Actions = { school }
---
+++ src/routes/register/school/+page.svelte
@@ -0,0 +1,258 @@
+<script lang="ts">
+	import { enhance } from '$app/forms'
+	import type { ActionData, PageServerData } from './$types'
+
+  export let data: PageServerData
+  const { country, regio, county, city } = data
+
+  let yesA = true
+  let yesB = false
+  let yesC = false
+  let yesD = false
+  let yesE = false
+  let yesF = false
+  let yesG = false
+  let yesH = false
+  let yesI = false
+  let yesJ = false
+  let yesK = false
+  let yesL = false
+  let yesM = false
+  let yesN = false
+  let yesO = true
+  let pageName="School Register"
+
+  //export let form: ActionData
+
+</script>
+<svelte:head>
+    <title> {pageName} </title>
+</svelte:head>
+
+<div class="grid">
+  <div class="rei">
+    <p>School Register</p>
+  </div>
+
+<form action="?/school" method="post" use:enhance>
+  <fieldset>
+    <legend>Location</legend>
+    <p class="notice">Please note: if country / region / county /city <i>does not exist</i>  in the list, <a class="aa" href="/registration/location">use this link</a>  before the registration. </p>
+    <div>
+      <label for="countr">Country</label>
+      <select name="countr" id="countr" >
+        {#each country as countr}
+          <option value="{countr.country_id}">{countr.name}</option>
+        {/each}
+      </select>
+    </div>
+    <div>
+      <label for="region">Region</label>
+      <select name="region" id="region" >
+        {#each regio as reg}
+          <option value="{reg.region_id}">{reg.name}</option>
+        {/each}
+      </select>
+    </div>
+    <div>
+      <label for="county">County</label>
+      <select name="county" id="county" >
+        {#each county as coun}
+          <option value="{coun.county_id}">{coun.name}</option>
+        {/each}
+      </select>
+    </div>
+    <div>
+      <label for="city">City</label>
+      <select name="city" id="city" >
+        {#each city as cit}
+          <option value="{cit.city_id}">{cit.name}</option>
+        {/each}
+      </select>
+    </div>
+  </fieldset>
+  <br>
+    <div>
+      <label for="om">OM id (only for Hungary)</label>
+      <input type="text" name="om" id="om" />
+    </div>
+    <div>
+      <label for="name">School name</label>
+      <input type="text" name="name" id="name" required />
+    </div>
+    <div>
+      <label for="zip">ZIP code</label>
+      <input type="text" name="zip" id="zip" required />
+    </div>
+    <div>
+      <label for="address">Address</label>
+      <input type="text" name="address" id="address" required />
+    </div>
+    <div>
+      <label for="dirname">Head of School</label>
+      <input type="text" name="dirname" id="dirname" required />
+    </div>
+    <div>
+      <label for="dirphone">School's phone number</label>
+      <input type="text" name="dirphone" id="dirphone" required />
+    </div>
+    <div>
+      <label for="email">School's email</label>
+      <input type="text" name="email" id="email" required />
+    </div>
+    <div>
+      <label for="website">Website</label>
+      <input type="text" name="website" id="website" required />
+    </div>
+    <div class="second">
+      <input type="checkbox" name="iskA" bind:checked={yesA} />
+      ÁLTALÁNOS ISKOLA
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskB" bind:checked={yesB} />
+      GIMNÁZIUM
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskC" bind:checked={yesC} />
+      SZAKGIMNÁZIUM
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskD" bind:checked={yesD} />
+      SZAKKÖZÉPISKOLA
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskE" bind:checked={yesE} />
+      SZAKISKOLA
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskF" bind:checked={yesF} />
+      TECHNIKUM
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskG" bind:checked={yesG} />
+      SZAKKÉPZŐ ISKOLA
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskH" bind:checked={yesH} />
+      ALAPFOKÚ MŰVÉSZETOKTATÁS
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskI" bind:checked={yesI} />
+      MŰVÉSZETI OKTATÁS
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskJ" bind:checked={yesJ} />
+      KÉSZSÉGFEJLESZTÉS
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskK" bind:checked={yesK} />
+      FEJLESZTŐ NEVELÉS-OKTATÁS
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskL" bind:checked={yesL} />
+      KIEGÉSZÍTŐ NEMZETISÉGI NYELVOKTATÁS
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskM" bind:checked={yesM} />
+      KOLLÉGIUM
+    </div>
+    <br>
+    <div class="second">
+      <input type="checkbox" name="iskN" bind:checked={yesN} />
+      HÍDPROGRAMOK
+    </div>
+    <br>
+    <fieldset>
+      <legend>Memo</legend>
+      <br>
+        <div class="second">
+          <input type="checkbox" name="coop" bind:checked={yesO} />
+          COOPERATION
+        </div>
+        <label for="message">Note:</label>
+        <textarea id="message"
+          name="message"
+          rows="4"
+          cols="50" >
+          </textarea>
+    </fieldset>
+
+  {#if (yesA == false && yesB == false && yesC == false && yesD == false && yesE == false && yesF == false && yesG == false && yesH == false && yesI == false && yesJ == false && yesK == false && yesL == false && yesM == false && yesN == false)}
+    <p class="error">One school type must be choosen.</p>
+  {/if}
+
+  <button class="btn" id="btn" type="submit">Register</button>
+</form>
+</div>
+
+<style>
+  .notice {
+    color: tomato;
+    padding: 2%;
+    text-align: center;
+    font-weight: bolder;
+    line-height: normal;
+  }
+
+  .aa {
+    color: #32BEA6;
+    font-style: italic;
+  }
+  fieldset {
+    position: relative;
+    padding: 6px;
+    border: 2px solid #32BEA6;
+    border-radius: 5px;
+    border-spacing: 2px;
+  }
+
+  label {
+    padding: 6px;
+  }
+  legend {
+    padding: 6px;
+  }
+
+  .rei p {
+    position: relative;
+    line-height: normal;
+    font-size: 140%;
+    font-weight: bold;
+  }
+
+  .grid {
+    padding: 35px 15px 0px 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-content: space-around;
+    width: 35%;
+    line-height: 85%;
+    grid-row: minmax(5px, auto);
+    margin: 10px auto 10px auto;
+    border: 2px solid #32BEA6;
+    border-radius: 5px;
+    border-spacing: 2px;
+    flex: 10 auto;
+  }
+  .grid input:checked {
+  background-color: #32BEA6;
+  }
+
+  .btn {
+    margin-bottom: 0;
+    background-color: #32BEA6;
+  }
+  </style>
