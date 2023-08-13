89e4936b0344be4a4da5ed4e6b4ac4860aca3c08
BiroNora <52859737+BiroNora@users.noreply.github.com>
2023-08-13 12:56:31 +0200

    Location register

---
+++ src/routes/register/location/+page.server.ts
@@ -0,0 +1,105 @@
+import { fail, redirect} from '@sveltejs/kit'
+import type { Action, Actions, PageServerLoad } from './$types'
+import { db } from '$lib/database'
+
+
+export const load: PageServerLoad = async () => {
+  const country = await db.country.findMany({
+    orderBy: { country_name: 'asc' },
+  })
+  const regio = await db.region.findMany({
+    orderBy: { region_name: 'asc' },
+  })
+  const county = await db.county.findMany({
+    orderBy: { county_name: 'asc' },
+  })
+  const city = await db.city.findMany({
+    orderBy: { city_name: 'asc' },
+  })
+  return {country, regio, county, city}
+}
+
+const location: Action = async ({ request }) => {
+  const data = await request.formData()
+  const country_name = String(data.get('country'))
+  const region_name = String(data.get('region'))
+  const county_name = String(data.get('county'))
+  const city_name = String(data.get('city'))
+
+  const countryifexists = await db.country.findUnique({
+    where: {country_name}
+  })
+
+  const regioncountry = await db.region.findUnique({
+    where: { region_name }
+  })
+
+  const countyregion = await db.county.findUnique({
+    where: { county_name }
+  })
+
+  const citycounty = await db.city.findUnique({
+    where: { city_name }
+  })
+
+  if ((countryifexists) && (regioncountry?.country_id == countryifexists?.country_id) && (countyregion?.region_id == regioncountry?.region_id) && (citycounty?.county_id == countyregion?.county_id) && (citycounty)) {
+    return fail(400, {school: true})
+  }
+
+  if (!countryifexists?.country_name) {
+    await db.country.create({
+      data: {
+        country_name,
+      }
+    })
+  }
+
+  const countryid = await db.country.findUnique({
+    where: {country_name}
+  })
+
+  const country_id = countryid?.country_id
+
+  if (!regioncountry?.region_name) {
+    await db.region.create({
+      data: {
+        region_name,
+        country_id
+      }
+    })
+  }
+
+  const regionid = await db.region.findUnique({
+    where: {region_name}
+  })
+
+  const region_id = regionid?.region_id
+
+  if (!countyregion?.county_name) {
+    await db.county.create({
+      data: {
+        county_name,
+        region_id
+      }
+    })
+  }
+
+  const countyid = await db.county.findUnique({
+    where: {county_name}
+  })
+
+  const county_id = countyid?.county_id
+
+  if (!citycounty?.city_name) {
+    await db.city.create({
+      data: {
+        city_name,
+        county_id
+      }
+    })
+  }
+
+  throw redirect(303, '/register/school')
+}
+
+export const actions: Actions = { location }
---
+++ src/routes/register/location/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import { enhance } from '$app/forms'
+	import type { PageServerData } from '../location/$types'
+
+  //export let data: PageServerData
+
+  let pageName="Location Register"
+</script>
+
+<svelte:head>
+    <title> {pageName} </title>
+</svelte:head>
+
+<div class="grid">
+  <div class="rei">
+    <p>Location Register</p>
+  </div>
+  <form action="?/location" method="post" use:enhance>
+  <fieldset>
+    <legend>New Location</legend>
+    <div>
+      <label for="country">Country</label>
+      <input type="text" name="country" id="country" required />
+    </div>
+    <div>
+      <label for="region">Region</label>
+      <input type="text" name="region" id="region" required />
+    </div>
+    <div>
+      <label for="county">County</label>
+      <input type="text" name="county" id="county" required />
+    </div>
+    <div>
+      <label for="city">City</label>
+      <input type="text" name="city" id="city" required />
+    </div>
+  </fieldset>
+  <br>
+  <button class="btn" id="btn" type="submit">Register</button>
+</form>
+</div>
+
+
+<style>
+  fieldset {
+    position: relative;
+    padding: 6px;
+    border: 2px solid #32BEA6;
+    border-radius: 5px;
+    border-spacing: 2px;
+  }
+
+  label {
+    padding: 6px;
+  }
+
+  legend {
+    padding: 6px;
+  }
+
+  .rei p {
+    position: relative;
+    line-height: normal;
+    font-size: 140%;
+    font-weight: bold;
+  }
+
+  .grid {
+    padding: 35px 15px 0px 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-content: space-around;
+    width: 35%;
+    line-height: 85%;
+    grid-row: minmax(5px, auto);
+    margin: 10px auto 10px auto;
+    border: 2px solid #32BEA6;
+    border-radius: 5px;
+    border-spacing: 2px;
+    flex: 10 auto;
+  }
+
+  .grid input:checked {
+  background-color: #32BEA6;
+  }
+
+  .btn {
+    margin-bottom: 0;
+    background-color: #32BEA6;
+  }
+</style>
