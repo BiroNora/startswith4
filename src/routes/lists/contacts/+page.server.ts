5e1ccfd24092a0b35936f2d6585e04531a2cb868
BiroNora <52859737+BiroNora@users.noreply.github.com>
2023-08-14 14:23:43 +0200

    Contacts wip

---
+++ src/routes/lists/contacts/+page.server.ts
@@ -0,0 +1,29 @@
+import { fail, redirect} from '@sveltejs/kit'
+import type { Action, Actions, PageServerLoad } from './$types'
+import { db } from '$lib/database'
+
+
+export const load: PageServerLoad = async () => {
+  const contact = await db.contact.findMany({
+    orderBy: { contact_name: 'asc' },
+  })
+  console.log(contact)
+  return {contact}
+}
+
+const contacts: Action = async ({ request }) => {
+  const data = await request.formData()
+  const contact_name = String(data.get('name'))
+  const contact_email = String(data.get('email'))
+
+  if (contact_name == '1') {
+      return fail(400, {school: true})
+  }
+  const regioncountry = await db.contact.findUnique({
+    where: { contact_email }
+  })
+
+  throw redirect(303, '/login')
+}
+
+export const actions: Actions = { contacts }
---
+++ src/routes/lists/contacts/+page.svelte
@@ -0,0 +1,129 @@
+<script lang="ts">
+	import { enhance } from '$app/forms'
+  import type { PageServerData } from './$types'
+
+
+  export let data: PageServerData
+  const { contact } = data
+
+  let pageName="Contacts"
+</script>
+
+  <svelte:head>
+    <title> {pageName} </title>
+</svelte:head>
+<div class="rounded-table">
+  <div >
+    <p class="rei">Contacts</p>
+  </div>
+  <br>
+  <div class="table">
+    <form class="scoob" action="?/contacts" method="post" use:enhance >
+      <table role="grid">
+        <thead>
+          <tr>
+            <th class="first fonty">Select</th>
+            <th class="cent fonty">Name</th>
+            <th class="cent fonty">Email</th>
+            <th class="cent fonty">Phone</th>
+            <th class="cent fonty">Note</th>
+          </tr>
+        </thead>
+          <tbody >
+            {#each contact as con}
+            <tr >
+              <td>
+                <div>
+                  <input type="checkbox" name="coop" />
+                </div>
+              </td>
+              <td class="cent fonts">{con.contact_name}</td>
+              <td class="cent fonts">{con.contact_email}</td>
+              <td class="cent fonts">{con.contact_phone}</td>
+              <td class="cent fonts">{con.note}</td>
+              <td class="cent fonts"><button class="dbc">
+                Update
+              </button></td>
+              <td ><button class="contrast outline cgb">
+                Delete
+              </button></td>
+            </tr>
+            {/each}
+        </tbody>
+      </table>
+    </form>
+    <button class="btn" id="btn" type="submit">Add to school</button>
+  </div>
+</div>
+
+<style>
+.table {
+  display: grid;
+  grid-template-columns:  auto;
+  margin: 0px 10px 10px 10px;
+  padding: 10px;
+  justify-content: stretch;
+  align-items: stretch;
+  border: 2px solid #32BEA6;
+  border-radius: 10px;
+  font-size: 140%;
+  column-gap: 5px;
+
+}
+
+.rounded-table {
+  margin: 5px;
+  border: 2px solid #32BEA6;
+  border-radius: 10px;
+}
+
+.rei {
+  padding-top: 40px;
+  padding-left: 20px;
+  padding-bottom: -5px;
+  position: relative;
+  line-height: normal;
+  font-size: 150%;
+  font-weight: bold;
+}
+
+.btn {
+  background-color: #32BEA6;
+}
+
+.dbc {
+  background-color: #32BEA6;
+}
+
+.cgb {
+  border: 2px solid tomato;
+}
+
+.first {
+  text-align: left;
+}
+
+.cent {
+  text-align: center;
+}
+
+.fonty {
+  color: #32BEA6;
+  font-weight: bolder;
+  font-style: italic;
+}
+
+.fonts {
+  font-weight: 500;
+  font-size: 100%;
+}
+
+.scoob {
+  max-height: 340px;
+  overflow-y: scroll;
+}
+
+.rounded-table input:checked {
+  background-color: #32BEA6;
+}
+</style>
