b9aed96f588cc125273e75ab066343b5293c9f73
BiroNora <52859737+BiroNora@users.noreply.github.com>
2023-09-05 21:02:02 +0200

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,31 @@
 <script lang="ts">
     import '@picocss/pico'
-  </script>
+</script>
 
 <nav aria-label="breadcrumb">
-    <ul>
-      <li>
-        <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="/register/user">User R</a>
-      </li>
-      <li>
-        <a href="/register/school">School R</a>
-      </li>
-      <li>
-        <a href="/register/location">Location R</a>
-      </li>
-      <li>
-        <a href="/register/contact">Contact R</a>
-      </li>
-      <li>
-        <a href="/register/event">Event R</a>
-      </li>
-      <li>
-        <a href="/lists/contacts">My Contact List</a>
-      </li>
-      <li>
-        <a href="/lists/schools">My School List</a>
-      </li>
-      <li>
-        <a href="/lists/events">My Event List</a>
-      </li>
-    </ul>
-  </nav>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/register/user">User R</a>
+    </li>
+    <li>
+      <a href="/register/school">School R</a>
+    </li>
+    <li>
+      <a href="/register/location">Location R</a>
+    </li>
+    <li>
+      <a href="/lists/schools">My School List</a>
+    </li>
+    <li>
+      <a href="/lists/events">My Event List</a>
+    </li>
+    <li>
+      <a href="/lists/contacts">My Contact List</a>
+    </li>
+  </ul>
+</nav>
 
 <slot />
