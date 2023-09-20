997f64e3296bd3d39c3ff1244346bd0786cc614c
BiroNora <52859737+BiroNora@users.noreply.github.com>
2023-09-20 10:43:27 +0200

    Others folder added

---
+++ src/routes/others/school_update/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  let regio = '';
+  let city = '';
+  let schooltype = '';
+  let duties = '';
+
+  function handleSubmit(event: Event) {
+    event.preventDefault();
+
+    // Store the input values in the page store
+    //$page.update((pageData) => ({
+    //  ...pageData,
+    //  regio,
+    //  city,
+    //  schooltype,
+    //  duties,
+    //  // Add more parameters as needed
+    //}));
+
+    // Navigate to a results page
+    //goto('/results'); // Use the `goto` function for navigation
+  }
+	let pageName = "Working On It"
+</script>
+
+<svelte:head>
+    <title> {pageName} </title>
+</svelte:head>
+
+<div class="grid">
+  <div class="rei">
+    <p>School Register</p>
+  </div>
+  <br>
+
+<form on:submit={handleSubmit}>
+  <label for="regio">Region:</label>
+  <input type="text" id="regio" bind:value={regio} />
+
+  <label for="city">City:</label>
+  <input type="text" id="city" bind:value={city} />
+
+  <label for="schooltype">School Type:</label>
+  <input type="text" id="schooltype" bind:value={schooltype} />
+
+  <label for="duties">Duties:</label>
+  <input type="text" id="duties" bind:value={duties} />
+
+  <!-- Add more input fields as needed -->
+
+  <button type="submit">Search</button>
+</form>
+</div>
+
+<style>
+	.rei p {
+    position: relative;
+    line-height: normal;
+    font-size: 140%;
+    font-weight: bold;
+  }
+
+  .grid {
+    padding: 35px 15px 0px 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-content: space-around;
+    width: 45%;
+    line-height: 85%;
+    grid-row: minmax(5px, auto);
+    margin: 10px auto 10px auto;
+    border: 2px solid #32BEA6;
+    border-radius: 5px;
+    border-spacing: 2px;
+    flex: 10 auto;
+  }
+
+  .grid input:checked {
+  background-color: #32BEA6;
+  }
+
+  .btn {
+    margin-bottom: 0;
+    background-color: #32BEA6;
+  }
+
+</style>
---
+++ src/routes/others/school_update/Modal.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	export let showModal: any // boolean
+
+	let dialog: any // HTMLDialogElement
+
+	$: if (dialog && showModal) dialog.showModal()
+
+  function confirmDelete() {
+    onDelete()
+    close()
+  }
+  </script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+  id="modal-example"
+>
+
+<article>
+    <h3>Az esemény adatai véglegesen törlődnek.</h3>
+    <footer>
+      <a href="#confirm"
+        role="button"
+        class="secondary aa"
+        data-target="modal-example"
+        on:click|self={() => dialog.confirmDelete()}>
+        Confirm
+      </a>
+      <a href="#cancel"
+        role="button"
+        class="secondary outline"
+        data-target="modal-example"
+        on:click|self={() => dialog.close()}>
+        Cancel
+      </a>
+    </footer>
+  </article>
+</dialog>
+
+
+<style>
+  .aa {
+    background-color: #32bea6;
+    border: 1em solide #32bea6;
+  }
+
+  .aa:hover {
+    background-color: #0ba38a;
+  }
+
+	dialog {
+		max-width: 32em;
+		border-radius: 0.2em;
+		border: none;
+		padding: 0;
+	}
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.3);
+	}
+
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+</style>
