669f0193aa98e1c734bd01625187176a0f83b4f8
BiroNora <52859737+BiroNora@users.noreply.github.com>
2023-12-03 16:51:02 +0100

    wip

--- src/routes/register/location/+page.svelte
+++ src/routes/register/location/+page.svelte
@@ -38,7 +38,7 @@
   {#if form?.error}
     <p class="error">{form?.message}</p>
 	{/if}
-  <button on:click class="btn" id="btn" type="submit">Register</button>
+  <button class="btn" id="btn" type="submit">Register</button>
 </form>
 </div>
 
