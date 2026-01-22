6d8a8ffc39bad83d40764e55377f6abb2dc308ea
BiroNora <52859737+BiroNora@users.noreply.github.com>
2026-01-22 14:11:20 +0100

    wip

--- tests/my_tests.spec.ts
+++ tests/my_tests.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test.skip('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await page.getByRole('link', { name: 'Log in' }).click();
   await page.getByRole('textbox', { name: 'Email' }).click();
